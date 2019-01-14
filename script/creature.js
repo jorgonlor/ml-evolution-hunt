59877c6a090f1876920a57aa73e758f259b440dc
jorge-PC\jorge <jorgonlor@gmail.com>
2019-01-14 22:38:39 +0000

    First commit

---
+++ script/creature.js
@@ -0,0 +1,306 @@
+/* 
+ * creature.js
+ * Created by Jorge Gonzalez, January, 10, 2019.
+ * Released under MIT License - see LICENSE file for details.
+ */
+
+"use strict";
+
+var MAX_TIME_WITHOUT_IMPROVING = 12;
+
+var BIAS_MUTATION_PROBABILITY = 0.05;
+var BIAS_MUTATION_CHANGE = 0.08;
+
+var DEBUG_MODE = false;
+var DEBUG_TIME = 0.5;
+var MANUAL_CONTROL = false;
+
+var CreatureType = {"first":1, "clone":2, "cross":3, "mutation":4, "new":5};
+
+class Food
+{
+    constructor(space, position, category)
+    {
+		let mass = 1;
+		this.radius = 9;
+		this.body = space.addBody(new cp.Body(mass, cp.momentForCircle(mass, 0, this.radius, cp.v(0, 0))));        
+		this.shape = space.addShape(new cp.CircleShape(this.body, this.radius, cp.v(0, 0)));
+		this.category = category;
+		this.body.p = new cp.v(position.x, position.y);
+		this.shape.setCollisionType(category);
+		this.shape.group = category;
+		this.shape.food = this;
+		this.alive = true;
+	}
+}
+
+class Creature
+{
+    constructor(space, position, category, creatureType)
+    {
+		let initializer = 'glorotNormal';
+
+		this.brain = tf.tidy(() => {
+			return tf.sequential({
+				layers: [
+				tf.layers.dense({units: 14, activation: 'linear', inputShape: [14], kernelInitializer: initializer, biasInitializer: initializer}),
+				tf.layers.leakyReLU({units: 9, kernelInitializer: initializer, biasInitializer: initializer}),	
+				tf.layers.leakyReLU({units: 4, kernelInitializer: initializer, biasInitializer: initializer}),		
+				tf.layers.dense({units: 2, /*activation: 'linear',*/ kernelInitializer: initializer, biasInitializer: initializer})]
+			});
+		});
+
+        this.radius = 5;
+		this.sight_distance = 250;
+		this.initPosition = new cp.v(position.x,position.y);
+		var mass = 1.5;
+		this.category = category;
+        this.body = space.addBody(new cp.Body(mass, cp.momentForCircle(mass, 0, this.radius, cp.v(0, 0))));        
+        this.shape = space.addShape(new cp.CircleShape(this.body, this.radius, cp.v(0, 0)));
+        this.shape.setElasticity(0.8);
+        this.shape.setFriction(0.1);
+        this.shape.setCollisionType(category);
+        //this.shape.setLayers(CREATURE_CATEGORY);
+        this.shape.group = category;
+        this.space = space;
+		this.body.p = new cp.v(position.x, position.y);
+		this.body.setAngle(0);
+        this.shape.body.rot = cp.v(1,0);
+		this.shape.creature = this;
+		this.creatureType = creatureType;
+
+		this.hitPoints = [];
+		this.eyePoints = [];
+        this.alive = true;
+
+		this.lapInitTime = self.now;
+		this.debugTime = self.now;
+        this.timeWhenMax = self.now;
+		this.maxFitness = 0.0;	
+
+		this.angle = cp.v.toangle(this.body.rot);
+		this.initAngle = this.angle;
+		this.spinCount = 0;
+		this.negativePropulsionCount = 0;
+		
+		this.showEyeTracing = true;
+		this.updateCycle = 0;
+
+		this.maxEnergy = 1700;
+		this.energy = this.maxEnergy;
+		this.foodCollectedCount = 0;
+		this.age = 0;
+    }
+
+    calculateEyesRotations()
+    {
+        var rot = this.shape.body.rot;
+
+        var rot_left_60 = cp.v(rot.x * 0.5 - rot.y * 0.8666, rot.x * 0.8666 + rot.y * 0.5);
+		var rot_left_30 = cp.v(rot.x * 0.8666 - rot.y * 0.5, rot.x * 0.5 + rot.y * 0.8666);
+		var rot_left_15 = cp.v(rot.x * 0.9659 - rot.y * 0.2588, rot.x * 0.2588 + rot.y * 0.9659);
+
+		var rot_right_15 = cp.v(rot.x * 0.9659 + rot.y * 0.2588, -rot.x * 0.2588 + rot.y * 0.9659);
+        var rot_right_30 = cp.v(rot.x * 0.8666 + rot.y * 0.5, -rot.x * 0.5 + rot.y * 0.8666);
+        var rot_right_60 = cp.v(rot.x * 0.5 + rot.y * 0.8666, -rot.x * 0.8666 + rot.y * 0.5);
+
+        return [rot_left_60, rot_left_30, rot_left_15, rot, rot_right_15, rot_right_30, rot_right_60];
+    }
+
+    update()
+    {
+		if(!this.alive) 
+			return;
+
+        var p = this.shape.body.p;
+		var now = self.now;
+		
+		if(this.alive)
+			this.age += 1;
+
+		this.energy -= 1;
+		if(this.energy <= 0) {
+			this.alive = false;
+			console.log("Death by starvation");
+		}
+		// if(this.updateCycle % 20 == 0) {
+		// 	let f = this.fitness();
+		// 	if(f > this.maxFitness * 1.02) {
+		// 		this.maxFitness = f;
+		// 		this.timeWhenMax = now;
+		// 	}
+		// }
+
+        // if(now - this.timeWhenMax > MAX_TIME_WITHOUT_IMPROVING * 1000) {
+        //     this.alive = false;
+        //     console.log("Death by not improving");
+		// }
+
+		var new_angle = cp.v.toangle(this.body.rot);
+		if(new_angle > this.initAngle && new_angle < this.initAngle + 0.4 && this.angle < this.initAngle && this.angle > this.initAngle - 0.4)
+			this.spinCount += 1;
+		if(new_angle < this.initAngle && new_angle > this.initAngle - 0.4 && this.angle > this.initAngle && this.angle < this.initAngle + 0.4)
+			this.spinCount -= 1;
+
+		if(this.spinCount > 2 || this.spinCount < -2) {
+			this.alive = false;
+			console.log("Death by spinning too much");
+		}
+		this.angle = new_angle;
+
+		if(this.negativePropulsionCount > 250) {
+			this.alive = false;
+			console.log("Death by too negative propulsion");
+		}
+        
+        var rots = this.calculateEyesRotations();
+        
+		this.hitPoints = [];
+		this.eyePoints = [];
+		var wall_eye_signals = [];
+		var food_eye_signals = [];
+        for(var i = 0; i < rots.length; ++i) {            
+            var p_near = vadd(p, vmult(rots[i], this.radius));
+			var p_far = vadd(p, vmult(rots[i], this.sight_distance));
+			
+			this.eyePoints.push([p_near, p_far]);
+
+			var wall_eye_signal = 0.0;
+			var food_eye_signal = 0.0;
+            var query = this.space.segmentQueryFirst(p_near, p_far, this.shape.group, this.shape.group);
+            if(query != null) {
+				var hitPoint = query.hitPoint(p_near, p_far);
+				this.hitPoints.push(hitPoint);
+				var signal = signal = (this.sight_distance - vdist(p, hitPoint)) / this.sight_distance;
+				if(query.shape.group == window.WALL_CATEGORY) {					
+					wall_eye_signal = signal;
+				}
+				else if(query.shape.group == window.FOOD_CATEGORY) {
+					food_eye_signal = signal;
+				}
+            }
+			wall_eye_signals.push(wall_eye_signal);
+			food_eye_signals.push(food_eye_signal);
+		}
+
+		if(MANUAL_CONTROL == true) return;
+		
+		//let vel = vlength(this.body.getVel()) / 100;
+		var eye_signals = wall_eye_signals.concat(food_eye_signals);
+		
+        var model_output = tf.tidy(()=> {
+			var p = this.brain.predict(tf.tensor2d([eye_signals]));
+			if(self.sim.useTanhEdit.checked)
+				return tf.tanh(p);
+			else
+				return p;			
+		});
+		
+        var thrust_control = model_output.get(0,0);
+        var turn_control = model_output.get(0,1);
+
+        this.body.applyImpulse( vmult(this.shape.body.rot, 10 * thrust_control), cp.v(0, 0));
+
+        this.body.applyImpulse( vmult(cp.v(-3, 0), turn_control), cp.v(0, 1));
+		this.body.applyImpulse( vmult(cp.v(3, 0), turn_control), cp.v(0, -1));
+
+		if(thrust_control < 0) 
+			++this.negativePropulsionCount;
+		else
+			this.negativePropulsionCount = 0;
+
+		
+		
+		if(DEBUG_MODE && now - this.debugTime > DEBUG_TIME * 1000) {
+			this.debugTime = now;
+			console.log("Thrust: " + thrust_control + ", Turn: " + turn_control);			
+		}
+
+		++this.updateCycle;
+		if(this.updateCycle >= 10000000)
+			this.updateCycle = 0;
+    }
+    
+    clone() {
+		var new_creature = new Creature(this.space, this.initPosition, this.category);
+		new_creature.creatureType = CreatureType.clone;
+
+		for(var i = 0; i < this.brain.layers.length; ++i) {
+
+			if(this.brain.layers[i].getWeights().length == 0) continue;
+
+			var weights = this.brain.layers[i].getWeights()[0].dataSync();
+			var new_weights = new_creature.brain.layers[i].getWeights()[0].dataSync();
+			for(var j = 0; j < weights.length; ++j) {
+				new_weights[j] = weights[j];
+			}
+
+			var bias = this.brain.layers[i].getWeights()[1].dataSync();
+			var new_bias = new_creature.brain.layers[i].getWeights()[1].dataSync();
+			for(var j = 0; j < bias.length; ++j) {
+				new_bias[j] = bias[j];
+			}
+		}
+
+		return new_creature;
+    }
+	
+	crossover(other) {
+		for(var i = 0; i < this.brain.layers.length; ++i) {
+
+			if(this.brain.layers[i].getWeights().length == 0) continue;
+
+			// Weights
+			var weights = this.brain.layers[i].getWeights()[0].dataSync();
+			var other_weights = other.brain.layers[i].getWeights()[0].dataSync();
+			var mid = weights.length / 2;
+			for(var j = mid; j < weights.length; ++j) {
+				weights[j] = other_weights[j];
+			}
+
+			// Bias
+			var bias = this.brain.layers[i].getWeights()[1].dataSync();
+			var other_bias = other.brain.layers[i].getWeights()[1].dataSync();
+			mid = bias.length / 2;
+			for(var j = mid; j < bias.length; ++j) {
+				bias[j] = other_bias[j];
+			}
+		}
+    }
+
+    mutate(mutationProbability, mutationChange) {
+		for(let i = 0; i < this.brain.layers.length; ++i) {
+
+			if(this.brain.layers[i].getWeights().length == 0) continue;
+
+			var weights = this.brain.layers[i].getWeights()[0].dataSync();
+			for(let j = 0; j < weights.length; ++j) {
+				if(Math.random() < mutationProbability) {
+					weights[j] += Math.random() * mutationChange - (mutationChange / 2.0);
+					// if(weights[j] > 1.3) weights[j] = 1.3;
+					// if(weights[j] < -1.3) weights[j] = -1.3;
+				}
+			}
+
+			var bias = this.brain.layers[i].getWeights()[1].dataSync();
+			for(let j = 0; j < bias.length; ++j) {
+				if(Math.random() < BIAS_MUTATION_PROBABILITY) {
+					bias[j] += Math.random() * BIAS_MUTATION_CHANGE - (BIAS_MUTATION_CHANGE / 2.0);
+					// if(bias[j] > 1.3) bias[j] = 1.3;
+					// if(bias[j] < -1.3) bias[j] = -1.3;
+				}
+			}
+		}
+		return this;
+    }
+
+    fitness() {
+		return 10 * this.age + this.foodCollectedCount * this.maxEnergy + this.energy;
+	}
+	
+	testFastPointInShape(p) {
+		if(Math.abs(p.x - this.body.p.x) < this.radius && Math.abs(p.y - this.body.p.y) < this.radius)
+			return true;
+		return false;
+	}
+}
---
+++ script/simulation.js
@@ -0,0 +1,307 @@
+/* 
+ * simulation.js
+ * Created by Jorge Gonzalez, January, 10, 2019.
+ * Released under MIT License - see LICENSE file for details.
+ */
+
+"use strict";
+
+var WALL_CATEGORY = 1;
+var CREATURE_CATEGORY = 2;
+var FOOD_CATEGORY = 3;
+
+var FOOD_PROBABILITY = 0.995;
+var MINIMUM_FOOD = 10;
+
+var WORLD_WIDTH = 1000;
+var WORLD_HEIGHT = 500;
+var WORLD_OFFSET = cp.v(150,100);
+var worldPoints = [
+ [WORLD_OFFSET.x, WORLD_OFFSET.y], 
+ [WORLD_OFFSET.x + WORLD_WIDTH, WORLD_OFFSET.y], 
+ [WORLD_OFFSET.x + WORLD_WIDTH, WORLD_OFFSET.y + WORLD_HEIGHT], 
+ [WORLD_OFFSET.x, WORLD_OFFSET.y + WORLD_HEIGHT]
+]; 
+
+var now = 1;
+
+class Simulation {
+
+    constructor(renderer) {
+        this.space = new cp.Space();
+        this.renderer = renderer;
+
+        //Configure space
+        var space = this.space;
+        space.iterations = 30;
+        space.gravity = cp.v(0, 0);
+        space.damping = 0.1;
+        space.sleepTimeThreshold = 0.5;
+        space.collisionSlop = 0.5;
+        space.addCollisionHandler(CREATURE_CATEGORY, WALL_CATEGORY, this.handleCreatureWallCollision, null, null, null);
+        space.addCollisionHandler(CREATURE_CATEGORY, FOOD_CATEGORY, this.handleCreatureFoodCollision, null, null, null);
+
+        this.createWorld(worldPoints);
+        this.creaturesInitPosition = cp.v(300,300);
+
+        this.generationCount = 0;
+        this.generationBest = 0;
+        this.generationBestPrevious = 0;  
+        this.generationStartTime = window.now;
+
+        this.bestLapTime = Number.MAX_SAFE_INTEGER;
+
+        this.eyeTracingCheckBox = document.getElementById('eyeTracing');
+        this.mutationProbabilityEdit = document.getElementById('mutProb');
+        this.mutationChangeEdit = document.getElementById('mutChange');
+        this.numCreaturesEdit = document.getElementById('numCreatures');
+        this.deadOnCollisionCheckBox = document.getElementById('deadOnCollision');
+        this.deadByOldCheckBox = document.getElementById('deadByOld');
+        this.useTanhEdit = document.getElementById('useTanh');
+        this.generationTimeToLiveEdit = document.getElementById('genTTL');
+
+        this.generationTimeToLive = this.generationTimeToLiveEdit.value;
+
+        // Creatures
+        this.creatures = [];
+        for(let i = 0; i < this.numCreaturesEdit.value; ++i) {
+            this.creatures.push(new Creature(this.space, this.creaturesInitPosition, CREATURE_CATEGORY, CreatureType.new));
+        }
+
+        this.food = [];
+        for(let i = 0; i < MINIMUM_FOOD; ++i) {
+            var posx = WORLD_OFFSET.x + 10 + (WORLD_WIDTH - 20) * Math.random();
+            var posy = WORLD_OFFSET.y + 10 + (WORLD_HEIGHT - 20) * Math.random();
+            this.food.push(new Food(this.space, cp.v(posx, posy), FOOD_CATEGORY));
+        }
+        
+        var self = this;
+        document.addEventListener('keydown', function(event) {
+            var creature = self.creatures[0];
+            var rot = creature.shape.body.rot;
+            if(event.keyCode == 87)  // w
+            {
+                creature.shape.body.applyImpulse( vmult(rot, 10), cp.v(0, 0));
+            }
+            if(event.keyCode == 83)  // s
+            {
+                creature.shape.body.applyImpulse( vmult(rot, -10), cp.v(0, 0));
+            }
+            if(event.keyCode == 65)  // a
+            {
+                creature.shape.body.applyImpulse( cp.v(-10, 0), cp.v(0, 1));
+                creature.shape.body.applyImpulse( cp.v(10, 0), cp.v(0, -1));
+            }
+            if(event.keyCode == 68)  // d
+            {
+                creature.shape.body.applyImpulse( cp.v(10, 0), cp.v(0, 1));
+                creature.shape.body.applyImpulse( cp.v(-10, 0), cp.v(0, -1));
+            }
+            if(event.keyCode == 80)  // p
+            {
+                console.log(self.creatures[0].fitness(self.checkpoints));
+            }
+        });
+    }
+
+
+    createWorld(worldPoints) {
+        var self = this;
+        let addWall = function(p0, p1) {
+            let wall = self.space.addShape(new cp.SegmentShape(self.space.staticBody, cp.v(p0[0], p0[1]), cp.v(p1[0], p1[1]), 0));
+            wall.setElasticity(1);
+            wall.setFriction(1);
+            wall.setCollisionType(WALL_CATEGORY);
+            wall.group = WALL_CATEGORY;
+            self.static_walls.push(wall); 
+        }
+
+        this.static_walls = [];
+
+        for (let i = 0; i < worldPoints.length; ++i) {
+            let p0 = worldPoints[i];
+            let p1 = worldPoints[(i + 1) % worldPoints.length];
+
+            addWall(p0, p1);        
+        }
+    }
+
+    handleCreatureWallCollision(arb, space) {
+        if(self.sim.deadOnCollisionCheckBox.checked) {
+            var c = arb.a.collision_type == CREATURE_CATEGORY ? arb.a : arb.b;
+            c.creature.alive = false;
+            c.creature.energy = 0;
+        }
+        //console.log("Death by collision");
+        return true;
+    }
+
+    handleCreatureFoodCollision(arb, space) {
+        if(arb.a.collision_type == FOOD_CATEGORY) {
+            var f = arb.a;
+            var c = arb.b;
+        }
+        else {
+            var f = arb.b;
+            var c = arb.a;
+        }
+        f.food.alive = false;
+        if(c.creature.alive) {
+            c.creature.energy = c.creature.maxEnergy;
+            ++c.creature.foodCollectedCount;
+            c.creature.spinCount = 0;
+        }
+        //console.log("Food collision");
+        return true;
+    }
+
+    run() {
+        this.running = true;
+        var self = this;
+        var lastTime = 0;
+        var step = function (time) {
+
+            self.update(time - lastTime);
+            self.draw();
+
+            lastTime = time;
+
+            if (self.running) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        step(0);
+    }
+
+    update(dt) { 
+        let now = self.now;
+        this.space.step(1/60);
+        self.now += 1000/60;
+
+        let mutationProbability = this.mutationProbabilityEdit.value;
+        let mutationChange = this.mutationChangeEdit.value;
+        let numCreatures = this.numCreaturesEdit.value;
+    
+        for(let i = 0; i < this.creatures.length; ++i) {
+            this.creatures[i].update();
+            this.creatures[i].showEyeTracing = this.eyeTracingCheckBox.checked;
+        }
+
+        if(!this.deadByOldCheckBox.checked)
+            this.generationStartTime = now;
+
+        if(now - this.generationStartTime > this.generationTimeToLive * 1000) {
+            this.creatures.forEach(c => {
+                if(c.alive) {
+                    c.alive = false;
+                    console.log("Death by old age");
+                }
+            });            
+        }
+
+        if(Math.random() > FOOD_PROBABILITY) {
+            var posx = WORLD_OFFSET.x + 10 + (WORLD_WIDTH - 20) * Math.random();
+            var posy = WORLD_OFFSET.y + 10 + (WORLD_HEIGHT - 20) * Math.random();
+            this.food.push(new Food(this.space, cp.v(posx, posy), FOOD_CATEGORY));
+        }
+
+        for(var i = this.food.length - 1; i >= 0 ; i--){
+            if(!this.food[i].alive) {
+                this.space.removeShape(this.food[i].shape);
+                this.space.removeBody(this.food[i].body);
+                this.food.splice(i,1);
+            }
+        }
+    
+        if(this.creatures.every(c => !c.alive)) {
+    
+            for(let i = 0; i < this.creatures.length; ++i) {
+                this.space.removeShape(this.creatures[i].shape);
+                this.space.removeBody(this.creatures[i].body);
+            }
+            
+            this.creatures.sort((c1, c2) => c2.fitness() - c1.fitness());
+            let numSurvivors = Math.floor(numCreatures / 3);
+            var survivors = this.creatures.slice(0, numSurvivors);
+            var first = survivors[0];
+            var second = survivors[1];
+            this.creatures = [];
+            
+            this.generationBestPrevious = this.generationBest;
+            this.generationBest = first.fitness();
+            console.log("Generation best: " + this.generationBest);
+    
+            // crosses
+            for(let i = 0; i < survivors.length - 1; ++i) {
+                for(let j = i + 1; j < survivors.length; ++j) {
+                    let cc = survivors[i].clone();
+                    cc.crossover(survivors[j]);
+                    cc.creatureType = CreatureType.cross;
+                    this.creatures.push(cc.mutate(0.02, 0.02));
+                }
+            }
+    
+            // clone first
+            let fclone = first.clone()
+            fclone.creatureType = CreatureType.first;
+            this.creatures.push(fclone)
+
+            // fine mutations
+            let numFimeMutations = numCreatures >= 10 ? 2 : 1;
+            for(let i = 0; i < numFimeMutations; ++i) {
+                let mutationOfFirst = first.clone().mutate(0.85, 0.05);
+                mutationOfFirst.creatureType = CreatureType.fineMutation;
+                this.creatures.push(mutationOfFirst);
+            }
+    
+            // coarse mutations
+            let creatures_len = this.creatures.length;
+            for(let i = 0; i < numCreatures - creatures_len; ++i) {
+                let mutationOfFirst = first.clone().mutate(mutationProbability, mutationChange);
+                mutationOfFirst.creatureType = CreatureType.mutation;
+                this.creatures.push(mutationOfFirst);
+            }
+
+            if(this.generationCount < 5)
+            {
+                this.creatures.push(new Creature(this.space, this.creaturesInitPosition, CREATURE_CATEGORY, CreatureType.new));
+                this.creatures.push(new Creature(this.space, this.creaturesInitPosition, CREATURE_CATEGORY, CreatureType.new));
+            }
+
+            for(let i = this.food.length; i < MINIMUM_FOOD; ++i) {
+                var posx = WORLD_OFFSET.x + 10 + (WORLD_WIDTH - 20) * Math.random();
+                var posy = WORLD_OFFSET.y + 10 + (WORLD_HEIGHT - 20) * Math.random();
+                this.food.push(new Food(this.space, cp.v(posx, posy), FOOD_CATEGORY));
+            }
+    
+            ++this.generationCount;
+            this.generationStartTime = now;
+            this.generationTimeToLive = this.generationTimeToLiveEdit.value;
+        }
+    }
+
+    draw() {
+        this.renderer.clear();
+
+        for(let i = 0; i < this.creatures.length; ++i)        
+            this.renderer.draw(this.creatures[i]);    
+            
+            for(let i = 0; i < this.food.length; ++i)        
+            this.renderer.draw(this.food[i]); 
+
+        for(let i = 0; i < this.static_walls.length; ++i)        
+            this.renderer.draw(this.static_walls[i]);            
+
+        let text = "Generation: " + this.generationCount;
+        text += "   Last: " + this.generationBest.toFixed(2);
+        text += "   Previous: " + this.generationBestPrevious.toFixed(2);
+        text += "   Remaining gen time: " + (this.deadByOldCheckBox.checked ? (Math.floor((this.generationTimeToLive * 1000 - (self.now - this.generationStartTime)) / 1000) + "s") : "--");
+        text += "   Best lap: " + ((this.bestLapTime != Number.MAX_SAFE_INTEGER) ? ((this.bestLapTime/1000).toFixed(2) + "s") : "N/A");        
+        
+        this.renderer.printInfo(text);
+    }
+};
+
+
+
+
